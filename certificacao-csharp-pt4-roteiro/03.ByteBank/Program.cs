835d0f71f51bd4b6386c23a3799b167fae3f248c
marcelooliveira <mclricardo@gmail.com>
2018-08-07 17:56:01 -0300

    .

--- certificacao-csharp-pt4-roteiro/03.ByteBank/Program.cs
+++ certificacao-csharp-pt4-roteiro/03.ByteBank/Program.cs
@@ -6,7 +6,59 @@ namespace _03.ByteBank
     {
         static void Main(string[] args)
         {
+            int fatorial = GetFatorial(5);
+            System.Console.WriteLine($"fatorial: {fatorial}");
+            Console.WriteLine();
             RelatorioClientes.ImprimirListagemClientes();
+        }
+
+        private static int GetFatorial(int numero)
+        {
+            //int contador = 1;
+            //int fatorial = 1;
+            //while (contador <= numero)
+            //{
+            //    fatorial *= contador;
+            //    contador++;
+            //}
+
+
+            //int fatorial = 1 * 2 * 3 * 4 * 5;
+            //System.Console.WriteLine($"fatorial: {fatorial}");
+
+
+
+            //int fatorial = 1;
+            //fatorial = fatorial * 1;
+            //fatorial = fatorial * 2;
+            //fatorial = fatorial * 3;
+            //fatorial = fatorial * 4;
+            //fatorial = fatorial * 5;
+
+
+            //int fatorial = 1;
+            //int contador = 1;
+            //fatorial = fatorial * contador;
+            //contador++;
+            //fatorial = fatorial * contador;
+            //contador++;
+            //fatorial = fatorial * contador;
+            //contador++;
+            //fatorial = fatorial * contador;
+            //contador++;
+            //fatorial = fatorial * contador;
+            //contador++;
+
+            int fatorial = 1;
+            int contador = 1;
+            while (contador <= numero)
+            {
+                fatorial = fatorial * contador;
+                contador++;
+            }
+            //System.Console.WriteLine($"fatorial: {fatorial}");
+
+            return fatorial;
         }
     }
 
--- certificacao-csharp-pt4-roteiro/03.ByteBank/RelatorioClientes.cs
+++ certificacao-csharp-pt4-roteiro/03.ByteBank/RelatorioClientes.cs
@@ -7,18 +7,45 @@ namespace _03.ByteBank
 {
     class RelatorioClientes
     {
+        //public static void ImprimirListagemClientes()
+        //{
+        //    using (var leitor = new StreamReader("Clientes.txt"))
+        //    {
+        //        // lê o cabeçalho
+        //        string linha = leitor.ReadLine();
+        //        Console.WriteLine(linha); //imprime cabeçalho
+
+        //        //lê primeira linha de dados
+        //        linha = leitor.ReadLine();
+        //        //verifica fim de arquivo
+        //        if (linha == null) { return; }
+
+        //        //imprime a primeira linha de dados
+        //        var campos = linha.Split(',');
+        //        Console.WriteLine(
+        //            $"{int.Parse(campos[0])}, {campos[1]}" +
+        //            $", {campos[2]}, {campos[3]}" +
+        //            $", {decimal.Parse(campos[4])}, {campos[5]}");
+        //    }
+        //}
+
         public static void ImprimirListagemClientes()
         {
             using (var leitor = new StreamReader("Clientes.txt"))
             {
-                leitor.ReadLine(); // pula o cabeçalho
-                Console.WriteLine($"id, nome, sobrenome, email, valor últ. empréstimo, ativo");
+                // lê o cabeçalho
+                string linha = leitor.ReadLine();
+                Console.WriteLine(linha); //imprime cabeçalho
 
-                string linha;
+                //lê primeira linha de dados e verifica fim de arquivo
                 while ((linha = leitor.ReadLine()) != null)
                 {
+                    //imprime a primeira linha de dados
                     var campos = linha.Split(',');
-                    Console.WriteLine($"{int.Parse(campos[0])}, {campos[1]}, {campos[2]}, {campos[3]}, {decimal.Parse(campos[4])}, {campos[5]}");
+                    Console.WriteLine(
+                        $"{int.Parse(campos[0])}, {campos[1]}" +
+                        $", {campos[2]}, {campos[3]}" +
+                        $", {decimal.Parse(campos[4])}, {campos[5]}"); 
                 }
             }
         }
